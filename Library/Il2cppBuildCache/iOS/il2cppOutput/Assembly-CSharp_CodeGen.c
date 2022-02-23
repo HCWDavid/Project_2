081a2f2735f470e9f75031825808ac33fdecc82c
David Wang <32970067+HCWDavid@users.noreply.github.com>
2022-02-22 22:12:27 -0600

    commit

---
+++ Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,59 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void VBTn::Start()
+extern void VBTn_Start_mEC3C49A515035AA33A5CA365BF1036B603510BB3 (void);
+// 0x00000002 System.Void VBTn::OnButtonPressed(Vuforia.VirtualButtonBehaviour)
+extern void VBTn_OnButtonPressed_mC125F35BBCE6E3A096E344B79A7BCCDBFA62C816 (void);
+// 0x00000003 System.Void VBTn::OnButtonReleased(Vuforia.VirtualButtonBehaviour)
+extern void VBTn_OnButtonReleased_mE9C1971C7897C15BDE61E834F6E3BFB2C54F1558 (void);
+// 0x00000004 System.Void VBTn::.ctor()
+extern void VBTn__ctor_m8109353F6FD2C39AF2CC26C5A649CE0C8F66DBE4 (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	VBTn_Start_mEC3C49A515035AA33A5CA365BF1036B603510BB3,
+	VBTn_OnButtonPressed_mC125F35BBCE6E3A096E344B79A7BCCDBFA62C816,
+	VBTn_OnButtonReleased_mE9C1971C7897C15BDE61E834F6E3BFB2C54F1558,
+	VBTn__ctor_m8109353F6FD2C39AF2CC26C5A649CE0C8F66DBE4,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	2551,
+	2064,
+	2064,
+	2551,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+Reference:
+https://www.youtube.com/watch?v=Ckw4RKKVE3k
